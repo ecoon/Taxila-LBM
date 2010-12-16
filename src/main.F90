6f905ca47445c48aadb4a800bbfa17ceeb7cdda9
ecoon <ecoon@users.noreply.github.com>
2010-12-16 13:43:51 -0700

    adding a file prefix to the options for i/o, to enable multiple outputs from multiple domains

--- src/main.F90
+++ src/main.F90
@@ -5,8 +5,8 @@
 !!!     version:
 !!!     created:         08 December 2010
 !!!       on:            11:48:19 MST
-!!!     last modified:   09 December 2010
-!!!       at:            15:16:54 MST
+!!!     last modified:   14 December 2010
+!!!       at:            12:16:41 MST
 !!!     URL:             http://www.ldeo.columbia.edu/~ecoon/
 !!!     email:           ecoon _at_ lanl.gov
 !!!
@@ -15,6 +15,7 @@
 #include "finclude/petscsysdef.h"
 #include "finclude/petscvecdef.h"
 #include "finclude/petscdmdef.h"
+
   program main
     use BC_module
     use LBM_module
@@ -36,26 +37,23 @@
     external initialize_state
     external initialize_walls
     type(lbm_type),pointer:: user
-    type(options_type),pointer:: options
-
-    
 
     ! --- setup environment
     call PetscInitialize(PETSC_NULL_CHARACTER, ierr)
     call constants_initialize(s)
-    options => OptionsCreate(s)
+    user => LBMCreate(PETSC_COMM_WORLD)
+    call OptionsSetSizes(user%options, s)
 
     ! --- read parameters
     infile = 'input_data'
-    call OptionsReadFile(options, infile)
-    call constants_set_from_options(options)
+    call OptionsReadFile(user%options, infile)
+    call constants_set_from_options(user%options)
 
     ! --- initialize memory
-    user => LBMCreate(PETSC_COMM_WORLD)
-    call LBMSetSizes(user, options%NX, options%NY, options%NZ, s, b)
-    user%bc%flags(:) = options%bc_flags(:)
+    call LBMSetSizes(user, user%options%NX, user%options%NY, user%options%NZ, s, b)
+    user%bc%flags(:) = user%options%bc_flags(:)
 
-    if (user%info%id.eq.0) call OptionsPrint(options)
+    if (user%info%id.eq.0) call OptionsPrint(user%options)
     ! --- initialize state
     ! walls
     if(user%info%id.eq.0) print*,'initialization of walls'
@@ -65,19 +63,19 @@
     call BCSetValues(user%bc, user%info, initialize_bcs)
 
     ! fi/state
-    if (options%new_simulation) then
+    if (user%options%new_simulation) then
        call LBMInitializeState(user, initialize_state)
        istep=1
     else
        call initialize_state_restarted(user%fi, user%rho, user%walls, &
-            options%istep, options%kwrite, user)
-       istep = options%istep
+            user%options%istep, user%options%kwrite, user)
+       istep = user%options%istep
     endif
 
     ! start lbm
     if(user%info%id.eq.0) print*,'calling lbm'
 
-    call LBMRun(user, istep, options%ntimes*options%npasses, options%kwrite) ! for the moment, this is crap
+    call LBMRun(user, istep, user%options%ntimes*user%options%npasses, user%options%kwrite) ! for the moment, this is crap
     call LBMDestroy(user, ierr)
     call PetscFinalize(ierr)
     stop
