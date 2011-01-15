146c0e361f0d23becd7b50529abc2517278f6c78
ecoon <ecoon@lanl.gov>
2011-01-14 19:00:56 -0700

    moving all tests initialize*.F to assorted initialize*.F90.  This includes many additions to ./common (and therefore links to these files from the test directories).  Also, this includes a major rewrite of flux/pressure bcs, which now can be set on the command line (i.e. in the input_data_petsc file).  See ./common/initialize_bcs_z.F90 for details.

---
+++ common/initialize_state_flushing.F90
@@ -0,0 +1,94 @@
+!!! ====================================================================
+!!!  Fortran-90-file
+!!!     author:          Ethan T. Coon
+!!!     filename:        initialize_state.F90
+!!!     version:         
+!!!     created:         14 January 2011
+!!!       on:            18:21:06 MST
+!!!     last modified:   14 January 2011
+!!!       at:            18:24:45 MST
+!!!     URL:             http://www.ldeo.columbia.edu/~ecoon/
+!!!     email:           ecoon _at_ lanl.gov
+!!!  
+!!! ====================================================================
+#define PETSC_USE_FORTRAN_MODULES 1
+#include "finclude/petscsysdef.h"
+#include "finclude/petscvecdef.h"
+#include "finclude/petscdmdef.h"
+
+  subroutine initialize_state(fi, rho, ux, uy, uz, walls, info)
+    use petsc
+    use Info_module
+    use constants
+    implicit none
+
+    ! input variables
+    type(info_type) info
+    PetscScalar,dimension(1:info%s,0:info%b, &
+         info%gxs:info%gxe, &
+         info%gys:info%gye, &
+         info%gzs:info%gze):: fi
+    PetscScalar,dimension(1:info%s, &
+         info%gxs:info%gxe, &
+         info%gys:info%gye, &
+         info%gzs:info%gze):: rho
+    PetscScalar,dimension(1:info%s, &
+         info%gxs:info%gxe, &
+         info%gys:info%gye, &
+         info%gzs:info%gze):: ux,uy,uz
+    PetscScalar,dimension(info%gxs:info%gxe, &
+         info%gys:info%gye, &
+         info%gzs:info%gze):: walls
+
+    ! local variables
+    PetscErrorCode ierr
+    logical,dimension(info%gxs:info%gxe, &
+         info%gys:info%gye, &
+         info%gzs:info%gze):: bound
+
+    PetscInt i,j,k,m ! local values
+    
+    ! initialize state
+    fi=0.0
+    ux=0.0
+    uy=0.0
+    uz=0.0
+
+    ! flushing experiement 
+    bound=.false.
+    do i=info%xs,info%xe
+       do j=info%ys,info%ye
+          do k=info%zs,info%ze
+             if(k.le.10) bound(i,j,k)=.true.
+          enddo
+       enddo
+    enddo
+    
+    ! set density
+    where(bound)
+       rho(1,:,:,:)=rho1(2)
+       rho(2,:,:,:)=rho2(1)
+    else where
+       rho(1,:,:,:)=rho1(1)
+       rho(2,:,:,:)=rho2(2)
+    end where
+    
+    where(walls.eq.1)
+       rho(1,:,:,:) = 0
+       rho(2,:,:,:) = 0
+    end where
+    
+    ! set state at equilibrium       
+    do i=info%xs,info%xe
+       do j=info%ys,info%ye
+          do k=info%zs,info%ze
+             do m=1,info%s
+                call LBMEquilf(fi(m,:,i,j,k),rho(m,i,j,k),ux(m,i,j,k), &
+                     uy(m,i,j,k), uz(m,i,j,k), alf(m), info%b)
+             enddo
+          enddo
+       enddo
+    enddo
+    
+    return
+  end subroutine initialize_state
---
+++ common/initialize_walls_duct.F90
@@ -0,0 +1,40 @@
+!!! ====================================================================
+!!!  Fortran-90-file
+!!!     author:          Ethan T. Coon
+!!!     filename:        initialize_walls.F90
+!!!     version:         
+!!!     created:         14 January 2011
+!!!       on:            17:26:22 MST
+!!!     last modified:   14 January 2011
+!!!       at:            18:41:21 MST
+!!!     URL:             http://www.ldeo.columbia.edu/~ecoon/
+!!!     email:           ecoon _at_ lanl.gov
+!!!  
+!!! ====================================================================
+#define PETSC_USE_FORTRAN_MODULES 1
+#include "finclude/petscsysdef.h"
+#include "finclude/petscvecdef.h"
+#include "finclude/petscdmdef.h"
+
+  subroutine initialize_walls(walls, filename, info)
+    use petsc
+    use Info_module
+    implicit none
+
+#include "lbm_definitions.h"
+!   input variables
+    type(info_type) info
+    PetscScalar,dimension(info%gxs:info%gxe, &
+         info%gys:info%gye, &
+         info%gzs:info%gze):: walls
+    character(len=MAXSTRINGLENGTH) filename
+
+    walls=0
+    
+    if (info%ys.eq.1) walls(:,1,:)=1
+    if (info%ye.eq.info%NY) walls(:,info%NY,:)=1
+    
+    if (info%zs.eq.1) walls(:,:,1)=1
+    if (info%ze.eq.info%NZ) walls(:,:,info%NZ)=1
+    return
+  end subroutine initialize_walls
