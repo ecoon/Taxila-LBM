d09f13242e11123490a060a744d030cdc171c049
ecoon <ecoon@users.noreply.github.com>
2011-03-17 16:24:02 -0600

    bug fix in public declarations

---
+++ src/lbm_constants.F90
@@ -0,0 +1,159 @@
+!!!==================================================================
+!!! Fortran-file
+!!!    author:        Ethan T. Coon
+!!!    filename:      constants.f
+!!!    version:
+!!!    created:       10 November 2010
+!!!      on:          13:04:46 MST
+!!!    last modified:  10 November 2010
+!!!      at:          13:04:46 MST
+!!!    URL:           http://www.ldeo.columbia.edu/~ecoon/
+!!!    email:         ecoon _at_ ldeo.columbia.edu
+!!!
+!!!==================================================================
+#define PETSC_USE_FORTRAN_MODULES 1
+#include "finclude/petscsysdef.h"
+  
+module LBM_Constants_module
+  implicit none
+
+  private 
+#include "lbm_definitions.h"
+
+  type, public:: constants_type
+     PetscInt s
+     PetscScalar g,g11,g22          ! constants for mobility forces
+     PetscScalar,pointer,dimension(:):: tau  ! relaxation times
+     PetscScalar,pointer,dimension(:,:):: gvt  ! gravity (i.e. body forces)
+     PetscScalar,pointer,dimension(:):: gw   ! fluid-solid interaction forces
+     PetscScalar,pointer,dimension(:):: mm   ! mass per number (rho = mm*n)
+     PetscScalar c_s2   ! sound speed squared
+     PetscScalar,pointer,dimension(:):: d_k   ! 
+     PetscScalar,pointer,dimension(:):: alpha_0   ! 
+     PetscScalar alpha_1
+     !PetscInt,pointer:: m(:,:)            ! transformation matrix
+     !PetscInt,pointer:: mt(:,:)           ! transpose of M
+     !PetscInt,pointer:: mmt(:)            ! diagonal M dot MT matrix 
+     PetscScalar,pointer,dimension(:,:):: tau_mrt  ! diagonal relaxation matrix
+
+  end type constants_type
+
+  public :: ConstantsCreate, &
+       ConstantsSetFromOptions, &
+       ConstantsView, &
+       ConstantsDestroy
+
+contains
+  function ConstantsCreate() result(constants)
+    type(constants_type),pointer:: constants
+
+    allocate(constants)
+    constants%g = 0
+    constants%g11 = 0
+    constants%g22 = 0
+    constants%c_s2 = 0
+
+    nullify(constants%tau)
+    nullify(constants%gvt)
+    nullify(constants%gw)
+    nullify(constants%mm)
+    nullify(constants%d_k)
+    nullify(constants%alpha_0)
+    nullify(constants%tau_mrt)
+
+  end function ConstantsCreate
+    
+  subroutine ConstantsSetFromOptions(constants, options, ierr)
+    use LBM_Options_module
+    
+    type(constants_type) constants
+    type(options_type) options
+    PetscErrorCode ierr
+
+    PetscInt nmax
+    PetscBool flag
+
+    constants%s = options%nphases
+    allocate(constants%tau(1:options%nphases))
+    allocate(constants%gvt(1:options%nphases,1:options%ndims))
+    allocate(constants%gw(1:options%nphases))
+    allocate(constants%mm(1:options%nphases))
+    allocate(constants%d_k(1:options%nphases))
+    allocate(constants%alpha_0(1:options%nphases))
+
+    ! defaults
+    constants%tau = 1.d0
+    constants%gvt = 0.d0
+    constants%gw = 0.d0
+    constants%mm = 1.d0
+    constants%c_s2 = 1.d0/3.d0
+
+    if (options%nphases > 1) then
+       call PetscOptionsGetReal(options%my_prefix, '-g', constants%g, flag, ierr)
+       call PetscOptionsGetReal(options%my_prefix, '-g11', constants%g11, flag, ierr)
+       call PetscOptionsGetReal(options%my_prefix, '-g22', constants%g22, flag, ierr)
+    end if
+
+    nmax = constants%s
+    call PetscOptionsGetRealArray(options%my_prefix, '-tau', constants%tau, nmax, &
+         flag, ierr)
+
+    nmax = constants%s
+    call PetscOptionsGetRealArray(options%my_prefix, '-gvt_x', &
+         constants%gvt(:,X_DIRECTION), nmax, flag, ierr)
+    nmax = constants%s
+    call PetscOptionsGetRealArray(options%my_prefix, '-gvt_y', &
+         constants%gvt(:,Y_DIRECTION), nmax, flag, ierr)
+
+    if (options%ndims > 2) then
+       nmax = constants%s
+       call PetscOptionsGetRealArray(options%my_prefix, '-gvt_z', &
+            constants%gvt(:,Z_DIRECTION), nmax, flag, ierr)
+       if (.not.flag) then
+          nmax = constants%s
+          call PetscOptionsGetRealArray(options%my_prefix, '-gvt', &
+               constants%gvt(:,Z_DIRECTION), nmax, flag, ierr)
+       end if
+    end if
+
+    nmax = constants%s
+    call PetscOptionsGetRealArray(options%my_prefix, '-gw', constants%gw, nmax, &
+         flag, ierr)
+    nmax = constants%s
+    call PetscOptionsGetRealArray(options%my_prefix, '-mm', constants%mm, nmax, &
+         flag, ierr)
+    constants%d_k = 1.d0 - 2.d0/(3.d0*constants%mm)
+  end subroutine ConstantsSetFromOptions
+  
+  subroutine ConstantsView(constants)
+    type(constants_type) constants
+    
+    print*, ' Physics:'
+    print*, '  gw =', constants%gw
+    print*, '  tau =', constants%tau
+    print*, '  gvt =', constants%gvt
+    print*, '  mm =', constants%mm
+    if (constants%s > 1) then
+       print*, '  g =', constants%g
+       print*, '  g11 =', constants%g11
+       print*, '  g22 =', constants%g22
+    end if
+  end subroutine ConstantsView
+
+  subroutine ConstantsDestroy(constants, ierr)
+    type(constants_type) constants
+    PetscErrorCode ierr
+    
+    if (associated(constants%tau)) deallocate(constants%tau)
+    if (associated(constants%gvt)) deallocate(constants%gvt)
+    if (associated(constants%gw)) deallocate(constants%gw)
+    if (associated(constants%mm)) deallocate(constants%mm)
+    if (associated(constants%d_k)) deallocate(constants%d_k)
+    if (associated(constants%alpha_0)) deallocate(constants%alpha_0)
+    !if (associated(constants%m)) deallocate(constants%m)
+    !if (associated(constants%mt)) deallocate(constants%mt)
+    !if (associated(constants%mmt)) deallocate(constants%mmt)
+    if (associated(constants%tau_mrt)) deallocate(constants%tau_mrt)
+  end subroutine ConstantsDestroy
+end module LBM_Constants_module
+  
---
+++ src/lbm_discretization_type.F90
@@ -0,0 +1,36 @@
+!!! ====================================================================
+!!!  Fortran-90-file
+!!!     author:          Ethan T. Coon
+!!!     filename:        lbm_discretization_type.F90
+!!!     version:         
+!!!     created:         15 March 2011
+!!!       on:            17:04:20 MDT
+!!!     last modified:   15 March 2011
+!!!       at:            17:04:47 MDT
+!!!     URL:             http://www.ldeo.columbia.edu/~ecoon/
+!!!     email:           ecoon _at_ lanl.gov
+!!!  
+!!! ====================================================================
+#define PETSC_USE_FORTRAN_MODULES 1
+#include "finclude/petscsysdef.h"
+
+module LBM_Discretization_Type_module
+  implicit none
+
+  private
+
+  type, public:: discretization_type
+     MPI_Comm comm
+     PetscInt name
+     PetscInt ndims
+     PetscInt b
+
+     PetscInt,pointer:: ci(:,:)
+     PetscScalar,pointer:: weights(:)
+     PetscInt,pointer:: m(:,:)
+     PetscInt,pointer:: mt(:,:)
+     PetscInt,pointer:: mmt(:)
+
+  end type discretization_type
+
+end module LBM_Discretization_Type_module
