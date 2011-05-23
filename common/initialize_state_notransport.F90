9bdb6220bf246fcce21dace0f60553a76126b68e
ecoon <ecoon@users.noreply.github.com>
2011-05-23 15:35:56 -0600

    bug fix in forgetting to nullfiy waypoints

---
+++ common/initialize_state_notransport.F90
@@ -0,0 +1,21 @@
+!!! ====================================================================
+!!!  Fortran-90-file
+!!!     author:          Ethan T. Coon
+!!!     filename:        initialize_state_notransport.F90
+!!!     version:         
+!!!     created:         20 April 2011
+!!!       on:            16:52:04 MDT
+!!!     last modified:   20 April 2011
+!!!       at:            16:52:18 MDT
+!!!     URL:             http://www.ldeo.columbia.edu/~ecoon/
+!!!     email:           ecoon _at_ lanl.gov
+!!!  
+!!! ====================================================================
+#define PETSC_USE_FORTRAN_MODULES 1
+#include "finclude/petscsysdef.h"
+#include "finclude/petscvecdef.h"
+#include "finclude/petscdmdef.h"
+
+  subroutine initialize_state_transport(fi, rho, u, walls, dist, phases, options)
+    ! do nothing
+  end subroutine initialize_state_transport
